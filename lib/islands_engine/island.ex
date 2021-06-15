ab10b8669dfc78224eaf3ba0bad6690177b55b51
p-sdk <paul.sdk@icloud.com>
2021-06-15 20:53:14 +0200

    Add Island.overlaps?/2

--- lib/islands_engine/island.ex
+++ lib/islands_engine/island.ex
@@ -35,4 +35,7 @@ defmodule IslandsEngine.Island do
         {:halt, {:error, :invalid_coordinate}}
     end
   end
+
+  def overlaps?(existing_island, new_island),
+    do: not MapSet.disjoint?(existing_island.coordinates, new_island.coordinates)
 end
