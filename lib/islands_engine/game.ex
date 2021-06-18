bf8bbfd40f302c04ae8470f2369ba2a357a857d6
p-sdk <paul.sdk@icloud.com>
2021-06-18 23:18:28 +0200

    Add Game.position_island/5

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -1,12 +1,17 @@
 defmodule IslandsEngine.Game do
   use GenServer
-  alias IslandsEngine.{Board, Guesses, Rules}
+  alias IslandsEngine.{Board, Coordinate, Guesses, Island, Rules}
+
+  @players [:player1, :player2]
 
   def start_link(name) when is_binary(name),
     do: GenServer.start_link(__MODULE__, name, [])
 
   def add_player(game, name) when is_binary(name),
     do: GenServer.call(game, {:add_player, name})
+
+  def position_island(game, player, key, row, col) when player in @players,
+    do: GenServer.call(game, {:position_island, player, key, row, col})
 
   def init(name) do
     player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
@@ -26,6 +31,39 @@ defmodule IslandsEngine.Game do
         {:reply, :error, state_data}
     end
   end
+
+  def handle_call({:position_island, player, key, row, col}, _from, state_data) do
+    board = player_board(state_data, player)
+
+    with {:ok, rules} <-
+           Rules.check(state_data.rules, {:position_islands, player}),
+         {:ok, coordinate} <-
+           Coordinate.new(row, col),
+         {:ok, island} <-
+           Island.new(key, coordinate),
+         %{} = board <-
+           Board.position_island(board, key, island) do
+      state_data
+      |> update_board(player, board)
+      |> update_rules(rules)
+      |> reply_success(:ok)
+    else
+      :error ->
+        {:reply, :error, state_data}
+
+      {:error, :invalid_coordinate} ->
+        {:reply, {:error, :invalid_coordinate}, state_data}
+
+      {:error, :invalid_island_type} ->
+        {:reply, {:error, :invalid_island_type}, state_data}
+    end
+  end
+
+  defp player_board(state_data, player),
+    do: Map.get(state_data, player).board
+
+  defp update_board(state_data, player, board),
+    do: Map.update!(state_data, player, fn player -> %{player | board: board} end)
 
   defp update_player2_name(state_data, name),
     do: put_in(state_data.player2.name, name)
