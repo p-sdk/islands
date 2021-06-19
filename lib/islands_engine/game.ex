3cc27e253a7a1783941063d3a7731a96e467bf77
p-sdk <paul.sdk@icloud.com>
2021-06-19 22:11:42 +0200

    Add Game.set_islands/2

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -13,6 +13,9 @@ defmodule IslandsEngine.Game do
   def position_island(game, player, key, row, col) when player in @players,
     do: GenServer.call(game, {:position_island, player, key, row, col})
 
+  def set_islands(game, player) when player in @players,
+    do: GenServer.call(game, {:set_islands, player})
+
   def init(name) do
     player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
     player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}
@@ -56,6 +59,20 @@ defmodule IslandsEngine.Game do
 
       {:error, :invalid_island_type} ->
         {:reply, {:error, :invalid_island_type}, state_data}
+    end
+  end
+
+  def handle_call({:set_islands, player}, _from, state_data) do
+    board = player_board(state_data, player)
+
+    with {:ok, rules} <- Rules.check(state_data.rules, {:set_islands, player}),
+         true <- Board.all_islands_positioned?(board) do
+      state_data
+      |> update_rules(rules)
+      |> reply_success({:ok, board})
+    else
+      :error -> {:reply, :error, state_data}
+      false -> {:reply, {:error, :not_all_islands_positioned}, state_data}
     end
   end
 
