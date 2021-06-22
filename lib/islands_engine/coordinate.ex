75d931baaf0ffb9be86d875813df95ded3afc437
p-sdk <paul.sdk@icloud.com>
2021-06-22 17:22:04 +0200

    Test Coordinate

--- lib/islands_engine/coordinate.ex
+++ lib/islands_engine/coordinate.ex
@@ -6,6 +6,20 @@ defmodule IslandsEngine.Coordinate do
 
   @board_range 1..10
 
+  @doc """
+  Creates a new Coordinate validating the given `row` and `col`.
+
+  ## Examples
+
+      iex> alias IslandsEngine.Coordinate
+      iex> Coordinate.new(1, 1)
+      {:ok, %IslandsEngine.Coordinate{col: 1, row: 1}}
+      iex> Coordinate.new(-1, 1)
+      {:error, :invalid_coordinate}
+      iex> Coordinate.new(11, 1)
+      {:error, :invalid_coordinate}
+  """
+
   def new(row, col) when row in @board_range and col in @board_range,
     do: {:ok, %Coordinate{row: row, col: col}}
 
