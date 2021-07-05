3a07d792465b9dadfa8f2762ea5e8db67f2951ec
p-sdk <paul.sdk@icloud.com>
2021-07-05 22:53:25 +0200

    Handle new_game

--- islands_interface/lib/islands_interface_web/channels/game_channel.ex
+++ islands_interface/lib/islands_interface_web/channels/game_channel.ex
@@ -5,4 +5,16 @@ defmodule IslandsInterfaceWeb.GameChannel do
   def join("game:" <> _player, _payload, socket) do
     {:ok, socket}
   end
+
+  def handle_in("new_game", _payload, socket) do
+    "game:" <> player = socket.topic
+
+    case GameSupervisor.start_game(player) do
+      {:ok, _pid} ->
+        {:reply, :ok, socket}
+
+      {:error, reason} ->
+        {:reply, {:error, %{reason: inspect(reason)}}, socket}
+    end
+  end
 end
